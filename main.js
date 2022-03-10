5939761bb89ec11c54bcf3e6526d857d796c5df7
Marino <marinobtt@gmail.com>
2022-03-10 20:09:58 -0300

    Insert onkeydown and onkeyup

--- main.js
+++ main.js
@@ -7,4 +7,17 @@ for (let c = 0; c < listaTeclas.length; c++) {
     tecla.onclick = function () {
         inputTel.value = inputTel.value + tecla.value;
     }
+
+
+    //Para utiliziar verificar todos os parâmetros do evento, basta inserir console.log('evento'). Ao clicar na tecla irá mostar no console do navegador todas as propriedades da tecla corresponte, inclusive o 'code:' utilizado aqui.
+    //O nome do evento é opcional, os mais utilizados são 'e' ou 'event'.
+    tecla.onkeydown = function (evento) {
+        if (evento.code === 'Enter' || evento.code === 'Space') {
+            tecla.classList.add('ativa')
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa')
+    }
 }
